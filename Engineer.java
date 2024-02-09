3ede4d91f3128b6da0f9d9f4f677012c05ab9337
Duotech B4 <duotechb3@gmail.com>
2024-02-08 19:44:08 -0500

    initial commit

---
+++ Engineer.java
@@ -0,0 +1,28 @@
+package myPack;
+
+public class Engineer {
+
+    private String name;
+    private String position;
+
+    public Engineer(String name, String position) {
+        this.name = name;
+        this.position = position;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+}
